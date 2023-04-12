0fdeeb25fe2efd1d3f56ee25b5ba956d3b4dfefb
faithll <869689491@qq.com>
2023-04-12 11:54:31 +0800

    动态改变大小已经测试完毕，准备接入MyWidget

--- src/Size.vue
+++ src/Size.vue
@@ -1,31 +1,56 @@
-<template>  
-    <div :style="{ backgroundColor: selectedColor, height: '100px', width: '100px' }">  
-      <!-- 点击的 div -->  
-      <div @click="handleClick" :style="{ backgroundColor: selectedColor, height: '50px', width: '50px' }">  
-        Click me  
-      </div>  
-    </div>  
-  </template>
+<template>
+    <div :style="{ backgroundColor: '#ff7722', height: h + 'px', width: w + 'px' }" style="position: relative;">
+        <!-- 点击的 div -->
+        <div @click="handleClick" ref="el" :style="{ backgroundColor: '#000fff', height: '30px', width: '30px' }" style="position: absolute;bottom: 0;right: 0;
+          ">
+            Click me
+            <br> x: {{ x }}
+            y: {{ y }}
+        </div>
+    </div>
+</template>
   
-  <script setup lang="ts">  
-  import { ref } from 'vue'  
-  import VueUse from '@vueuse/core'
-  
- 
-  
-    //   // 使用 vueuse 的 useMouse 和 useMousePressed 钩子来捕获鼠标事件  
-    //   const mouse = VueUse.useMouse()  
-    //   const mousePressed = VueUse.useMousePressed()
-  
-    //   // 记录点击位置  
-    //   const clickPos = mousePressed()
-  
-    //   // 拖动时鼠标的位置  
-    //   const dragPos = mousePressed.getDragPos()
-  
-    //   // 获取 div 的实例  
-    //   const div = mount.getDOMNode().getElementById('div')
-  
- 
-  </script>  
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import { useMousePressed, whenever, useMouse } from '@vueuse/core'
+const h = ref(100)
+const w = ref(100)
+const { x, y, sourceType } = useMouse()
+const el = ref(null)
+const oldx = ref(w.value)
+const oldy = ref(h.value)
+const { pressed } = useMousePressed({ target: el })
+watch(pressed,()=>{
+    console.log("eeee "+pressed.value)
+    if(pressed.value){
+        oldx.value = x.value;
+        oldy.value = y.value;
+    }else {
+        // console.log("x "+(x.value - oldx.value))
+        // console.log("y "+(y.value - oldy.value))
+    }
+  })
+const changeSize = (xx, yy) => {
+    if (pressed.value) {
+        h.value = h.value + (y.value - oldy.value)
+        w.value = w.value + (x.value - oldx.value)
+
+        console.log("x " + (x.value - oldx.value))
+        console.log("y " + (y.value - oldy.value))
+        oldx.value = x.value;
+        oldy.value = y.value;
+    } else {
+
+    }
+}
+watch(x, () => {
+    changeSize(x, y)
+})
+watch(y, () => {
+    changeSize(x, y)
+})
+
+
+
+</script>  
   
