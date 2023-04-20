ca0fbc6b501e25d5fca407215582f28928c53ffc
小白 <869689491@qq.com>
2023-04-20 23:04:28 +0800

    右键菜单添加 未完成

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import '@imengyu/vue3-context-menu/lib/vue3-context-menu.css'
+import ContextMenu from '@imengyu/vue3-context-menu'
 
 import './index.css'
 
 const app = createApp(App)
-
+app.use(ContextMenu)
 app.mount('#app')
+// https://imengyu.top/pages/vue3-context-menu-docs/guide/useage.html
